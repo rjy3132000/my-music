4b234955edeb68dcffd3b465ba7f15a2bd033ba0
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2023-05-11 16:18:02 +0530

    design WaveAnimation and Controls

--- src/components/AudioPlayer/Control.js
+++ src/components/AudioPlayer/Control.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Control.css";
+import { IconContext } from "react-icons";
+import { FaPause } from "react-icons/fa";
+import { IoPlaySkipBack, IoPlaySkipForward, IoPlay } from "react-icons/io5";
 
-const Control = () => {
+export default function Controls({
+    isPlaying,
+    setIsPlaying,
+    handleNext,
+    handlePrev,
+}) {
     return (
-        <div>Control</div>
-    )
+        <IconContext.Provider value={{ size: "35px", color: "#C4D0E3" }}>
+            <div className="controls-wrapper flex">
+                <div className="action-btn flex" onClick={handlePrev}>
+                    <IoPlaySkipBack />
+                </div>
+                <div
+                    className={
+                        isPlaying ? "play-pause-btn flex active" : "play-pause-btn flex"
+                    }
+                    onClick={() => setIsPlaying(!isPlaying)}
+                >
+                    {isPlaying ? <FaPause /> : <IoPlay />}
+                </div>
+                <div className="action-btn flex" onClick={handleNext}>
+                    <IoPlaySkipForward />
+                </div>
+            </div>
+        </IconContext.Provider>
+    );
 }
-
-export default Control
